ee72d0406249175dd3de693455cc4844b9481bdd
Jan Matas <janmatasc@gmail.com>
2023-08-10 16:43:12 +0200

    Change MatrixExponentiation.cpp

--- MatrixExponentiation.cpp
+++ MatrixExponentiation.cpp
@@ -1,35 +1,37 @@
-struct matrix {
-  int n, m;
-  vector<vector<int>> a;
-  matrix(int n_, int m_) {
+struct Matrix {
+  int n;
+  vector<vector<int>> m;
+  
+  Matrix(int n_) {
     n = n_; 
-    m = m_;
-    a.resize(n, vector<int>(m));
+    m = vector<vector<int>>(n, vector<int>(n));
   }
   
-  matrix operator *(matrix other) {
-    matrix C = matrix(N, N);
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < N; j++) {
-        for (int k = 0; k < N; k++) {
-          C.a[i][j] += a[i][k] * other.a[k][j];
+  Matrix(int n_, int val) {
+    n = n_;
+    m = vector<vector<int>>(n, vector<int>(n));
+    for (int i = 0; i < n; i++) m[i][i] = val;
+  }
+  
+  Matrix operator *(Matrix& other) {
+    Matrix res(n);
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        for (int k = 0; k < n; k++) {
+          res.m[i][j] += m[i][k] * other.m[k][j];
         }
       }
     }
-    return C;
+    return res;
   }
 };
 
-matrix pow(matrix& A, int b) {
-  matrix C(N, N);
-  for (int i = 0; i < N; i++) C.a[i][i] = 1;
-  
+Matrix matpow(Matrix& a, int b) {
+  Matrix res(a.n, 1);
   while (b > 0) {
-    if (b & 1) {
-      C = C * A;
-    }
-    A = A * A;
+    if (b & 1) res = res * a;
+    a = a * a;
     b >>= 1;
   }
-  return C;
+  return res;
 }
