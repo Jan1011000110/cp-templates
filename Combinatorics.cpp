baa477db2988bbe5f713dcd58496443ad663853e
Jan Matas <janmatasc@gmail.com>
2023-09-04 21:06:02 +0200

    Change combi

--- Combinatorics.cpp
+++ Combinatorics.cpp
@@ -2,27 +2,27 @@ const ll N = 2e5+5, INF = 1e18, MOD = 1e9+7;
 
 vector<int> fact(N), inv_fact(N);
 
-int mul(int a, int b) {
+int mult(int a, int b) {
   return (a * b) % MOD;
 }
 int add(int a, int b) {
   return (a + b) % MOD;
 }
 int mpow(int a, int b) {
-  return !b ? 1 : mul(mpow(mul(a, a), b / 2), (b & 1 ? a : 1));
+  return not b ? 1 : mult(mpow(mult(a, a), b / 2), (b & 1 ? a : 1));
 }
 int inv(int a) {
   return mpow(a, MOD - 2);
 }
 int C(int a, int b) {
   if (b > a) return 0;
-  return mul(fact[a], mul(inv_fact[b], inv_fact[a - b]));
+  return mult(fact[a], mult(inv_fact[b], inv_fact[a - b]));
 }
 
 void init() {
   fact[0] = inv_fact[0] = 1;
   for (int i = 1; i < N; i++) {
-    fact[i] = mul(fact[i - 1], i);
+    fact[i] = mult(fact[i - 1], i);
     inv_fact[i] = inv(fact[i]);
   }
 }
