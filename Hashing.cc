ca3690d54c83e55143df5d5039d939133e7a97d2
Jan <janmataspython@gmail.com>
2023-12-08 14:17:50 +0100

    Modify Hash.cc so it can hold 2 hashes

--- Hashing.cc
+++ Hashing.cc
@@ -1,13 +1,13 @@
 struct Hash {
-	long long B = 32478
-	long long M = 1027799999;
+	long long B;
+	long long M;
 
 	int n;
-	vector<int> pw;
-	vector<int> hash;
+	vector<long long> pw;
+	vector<long long> hash;
 
-	void build(string s) {
-		n = int(s.size());
+	void build(string s, long long B = 329483, long long M = 1027799999) {
+		n = (int) s.size();
 		pw = vector<long long>(n + 1);
 		hash = vector<long long>(n + 1);
 
@@ -23,5 +23,34 @@ struct Hash {
 	
 	long long get_hash(int left, int right) {
 		return ((hash[right + 1] - (hash[left] * pw[right - left + 1]) % M) + M) % M;
+	}
+
+	long long get_hash() {
+		return get_hash(0, n - 1);
+	}
+};
+
+struct MultiHash {
+	int COUNT_HASHES = 2;
+
+	vector<long long> POLYS = {388293, 68};
+	vector<long long> MODS = {1027799999, 998244353};
+
+	int n;
+	vector<Hash> hashes(COUNT_HASHES);
+
+	void build(string s) {
+		n = (int) s.size();
+		for (int i = 0; i < COUNT_HASHES; ++i) {
+			hashes[i].build(s, POLYS[i], MODS[i]);
+		}
+	}
+
+	pair<long long, long long> get_hash(int left, int right) {
+		return {hashes[0].get_hash(left, right), hashes[1].get_hash(left, right)};
+	}
+
+	pair<long long, long long> get_hash() {
+		return get_hash(0, n - 1);
 	}
 };
